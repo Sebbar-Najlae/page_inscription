ebc4b50ddb0651cf5399c296ca9a65851b7602df
sebbar najlae <sebbar.najlae.dev@gmail.com>
2021-03-24 15:12:10 +0100

    Prototype

---
+++ Prototype/traitement.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>
+Test Formulaire PHP
+</title>
+</head>
+<body>
+
+
+<h1>Bonjour,  <?php echo $_POST['nom'] ?></h1>
+<h2>Vous semblez avoir  <?php echo $_POST['age'] ?></h2>
+
+
+
+</body>
+
+</html>
+
